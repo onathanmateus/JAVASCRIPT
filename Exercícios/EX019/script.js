b4a6ce602345a62221486bbb62f0e62d60ae47c6
Nathan Mateus <nathanmateudeo@hotmail.com>
2023-01-04 08:43:35 -0300

    Update script.js

--- Exercícios/EX019/script.js
+++ Exercícios/EX019/script.js
@@ -1,17 +1,4 @@
-function tabuada(){
-    var num = document.getElementById("txtn");
-    var tab = document.getElementById("seltab");
-    if (num.value.length == 0){
-        alert("[ERRO] Por favor, digite um número!")
-    }else{
-        var n = Number(num.value)
-        var c = 1
-        tab.innerHTML = ""
-        for(c = 1; c <= 10; c++){
-            var item = document.createElement("option")
-            item.text = `${n} X ${c} = ${n*c}`
-            tab.appendChild(item)
-        }
-    }
+function soma(n1=0, n2=0){
+    return n1 + n2;
 }
-    
+console.log(soma(7, 6));
