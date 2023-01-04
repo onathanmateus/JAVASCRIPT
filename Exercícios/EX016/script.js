d0b01532ee84487e409a511eb609f0193072b67c
Nathan Mateus <nathanmateudeo@hotmail.com>
2023-01-04 08:43:16 -0300

    Update script.js

--- Exercícios/EX016/script.js
+++ Exercícios/EX016/script.js
@@ -1,13 +1,7 @@
-var c = 1
+var num = [5, 8, 2, 6, 4]
 
-while (c <= 6){
-    console.log(`Passo ${c}`)
-    c++
-}
-
-/*
-do{
-    console.log(`Passo ${c}`)
-    c++
-} while (c <=6)
-*/
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
