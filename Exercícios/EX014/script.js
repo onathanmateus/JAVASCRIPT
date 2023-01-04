09759675430fc16fc59b46283cfb720ca860fb56
Nathan Mateus <nathanmateudeo@hotmail.com>
2023-01-04 08:42:43 -0300

    Update script.js

--- Exercícios/EX014/script.js
+++ Exercícios/EX014/script.js
@@ -1,19 +1,13 @@
-function carregar(){
-    var msg = window.document.getElementById("msg");
-    var img = window.document.getElementById("imagem");
-    var data = new Date()
-    var hora = data.getHours()
+var c = 1
 
-    msg.innerHTML = `Agora são ${hora} horas.`;
-    if (hora >=0 && hora < 12){
-        img.src = 'imagemmanha.png'
-        document.body.style.backgroundColor = '#fec969'
-    } else if (hora >=12 && hora < 18){
-        img.src = 'imagemtarde.png'
-        document.body.style.backgroundColor = '#ac6e66'
-    }else{
-        img.src = 'imagemnoite.png'
-        document.body.style.backgroundColor = '#62445c'
-    }
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
 }
 
+/*
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while (c <=6)
+*/
