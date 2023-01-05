256a39ab8e283de3f0a42b930d005e7e1d1b20e3
Nathan Mateus <nathanmateudeo@hotmail.com>
2023-01-05 17:19:48 -0300

    create EX005

---
+++ Exercises/Pack of exercises 2/EX005/js/script.js
@@ -0,0 +1,12 @@
+var nome = 'Nathan';
+var sobrenome = 'Mateus';
+var nomecompleto = nome + ' ' + sobrenome;
+
+
+console.log(nome);
+console.log(typeof nome);
+console.log(nomecompleto);
+
+console.log('Ele disse "Olá"');
+console.log("Ele disse 'Olá'");
+
