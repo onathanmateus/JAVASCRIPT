ea31e31f8c54d5cebffe599215a6a68a9d71093e
Nathan Mateus <nathanmateudeo@hotmail.com>
2023-01-05 10:01:14 -0300

    Create script.js

---
+++ Exercises/Pack of exercises 2/EX002/js/script.js
@@ -0,0 +1,18 @@
+console.log("Primeiro!");
+
+console.log("Último!");
+
+var a = 1;
+
+a = 'texto';
+
+console.log(a);
+
+//Este é um comentário em linha 
+
+/*
+Este é 
+um comentário em
+múltiplas 
+linhas
+*/
