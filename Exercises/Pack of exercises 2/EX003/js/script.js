e8d2e2182f3200f864c0cdce286239e6f275fd11
Nathan Mateus <nathanmateudeo@hotmail.com>
2023-01-05 10:28:06 -0300

    Create script.js

---
+++ Exercises/Pack of exercises 2/EX003/js/script.js
@@ -0,0 +1,23 @@
+var teste = 1;
+var nome = 'João'
+
+console.log(teste);
+
+teste = 'Nathan';
+
+console.log(teste);
+console.log(nome);
+
+//Podemos criar variáveis usando "$" e "_" antes do nome da variável
+var $nome = 'ASD1';
+var _nome = 'ASD2';
+console.log($nome);
+console.log(_nome);
+
+//Podemos criar variáveis adicionando um número qualquer depois do nome da variável
+var nome5 = 'ASD3';
+console.log(nome5);
+
+//Dica: se o nome da variável for grande, é bom deixar a primeira letra de cada palavra em maiúculo, isso melhora a leitura do código
+var MeuPrimeiroNome = 'Nathan';
+console.log(MeuPrimeiroNome);
