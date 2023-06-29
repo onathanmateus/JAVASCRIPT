06175c26c5c35ec7305d1a290ab0c087bcde3e61
Nathan Mateus <117992913+onathanmateus@users.noreply.github.com>
2023-06-29 09:43:16 -0300

    feat: EX006/index.js finalizado

--- Pack of exercises 2/EX006/index.js
+++ Pack of exercises 2/EX006/index.js
@@ -1,4 +1,23 @@
 function meuEscopo() {
   const form = document.querySelector("form");
-  form.addEventListener("submit");
+  const pessoas = [];
+
+  function receiveEvent(event) {
+    event.preventDefault();
+    const nome = form.querySelector(".nome").value;
+    const sobrenome = form.querySelector(".sobrenome").value;
+    const peso = form.querySelector(".peso").value;
+    const altura = form.querySelector(".altura").value;
+
+    pessoas.push({
+      nome: nome,
+      sobrenome: sobrenome,
+      peso: peso,
+      altura: altura,
+    });
+
+    console.log(pessoas);
+  }
+  form.addEventListener("submit", receiveEvent);
 }
+meuEscopo();
