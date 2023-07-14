3c29eb05694d1e80d44ce5738b525ab05d3e29a6
Nathan Mateus <117992913+onathanmateus@users.noreply.github.com>
2023-07-14 10:01:56 -0300

    feat: main.js updated

--- Pack of exercises 2/EX007/assets/js/main.js
+++ Pack of exercises 2/EX007/assets/js/main.js
@@ -2,9 +2,17 @@ const form = document.querySelector("#form");
 
 addEventListener("submit", function (e) {
   e.preventDefault();
+  const peso = e.target.querySelector("#peso");
+  console.log(peso);
 });
+
+function createP() {
+  const p = document.createElement("p");
+  return p;
+}
 
 function setResult(msg) {
   const resultado = document.querySelector("#resultado");
   resultado.innerHTML = "";
+  const p = createP();
 }
