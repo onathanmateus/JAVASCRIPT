96774f675ad29bcf43ad5b644cbdc47b8b7a5fe4
Nathan Mateus <117992913+onathanmateus@users.noreply.github.com>
2023-07-17 16:34:54 -0300

    feat: main.js and index.html updated

--- Pack of exercises 2/EX007/assets/js/main.js
+++ Pack of exercises 2/EX007/assets/js/main.js
@@ -2,17 +2,39 @@ const form = document.querySelector("#form");
 
 addEventListener("submit", function (e) {
   e.preventDefault();
-  const peso = e.target.querySelector("#peso");
-  console.log(peso);
+  const inputWeight = e.target.querySelector("#weight");
+  const inputHeight = e.target.querySelector("#height");
+  const weight = Number(inputWeight.value);
+  const height = Number(inputHeight.value);
+
+  if (!weight) {
+    setResult("Peso inválido", false);
+    return;
+  }
+  if (!height) {
+    setResult("Altura inválida", false);
+    return;
+  }
+
+  const imc = getImc(weight, height);
+  console.log(imc);
 });
+
+function getImc(weight, height) {
+  const imc = weight / Math.pow(height, 2);
+  return imc.toFixed(2);
+}
 
 function createP() {
   const p = document.createElement("p");
   return p;
 }
 
-function setResult(msg) {
-  const resultado = document.querySelector("#resultado");
-  resultado.innerHTML = "";
+function setResult(msg, isValid) {
+  const result = document.querySelector("#result");
+  result.innerHTML = "";
+
   const p = createP();
+  p.innerHTML = msg;
+  result.appendChild(p);
 }
