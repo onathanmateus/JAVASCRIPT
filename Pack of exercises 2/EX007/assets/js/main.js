c147af2370ae2a80b194a70e9e804ef2f247be93
Nathan Mateus <117992913+onathanmateus@users.noreply.github.com>
2023-07-24 09:40:39 -0300

    feat: EX007 finished (IMC calculator)

--- Pack of exercises 2/EX007/assets/js/main.js
+++ Pack of exercises 2/EX007/assets/js/main.js
@@ -17,14 +17,43 @@ addEventListener("submit", function (e) {
   }
 
   const imc = getImc(weight, height);
-  console.log(imc);
+  const nivelImc = getLevelImc(imc);
+  const msg = `O seu IMC é ${imc} (${nivelImc}).`;
+  setResult(msg, true);
+
+  console.log(nivelImc);
 });
+
+function getLevelImc(imc) {
+  const level = [
+    "Abaixo do peso",
+    "Peso normal",
+    "Sobrepeso",
+    "Obesidade grau 1",
+    "Obesidade grau 2",
+    "Obesidade grau 3",
+  ];
+
+  if (imc >= 39.9) return level[5];
+  if (imc >= 34.9) return level[4];
+  if (imc >= 29.9) return level[3];
+  if (imc >= 24.9) return level[2];
+  if (imc >= 18.5) return level[1];
+  if (imc <= 18.5) return level[0];
+}
 
 function setResult(msg, isValid) {
   const result = document.querySelector("#result");
   result.innerHTML = "";
 
   const p = createP();
+
+  if (isValid) {
+    p.classList.add("paragrafo-resultado");
+  } else {
+    p.classList.add("bad");
+  }
+
   p.innerHTML = msg;
   result.appendChild(p);
 }
