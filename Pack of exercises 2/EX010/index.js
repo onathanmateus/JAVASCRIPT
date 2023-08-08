4093d9011ef07e5725155ae28610eba6e32ee4ad
Nathan Mateus <117992913+onathanmateus@users.noreply.github.com>
2023-08-08 09:35:56 -0300

    Feat: EX010 created

---
+++ Pack of exercises 2/EX010/index.js
@@ -0,0 +1,12 @@
+const fizzBuzz = (number) => {
+  if (typeof number !== "number") return number;
+  if (number % 3 === 0 && number % 5 === 0) return "FizzBuzz";
+  if (number % 3 === 0) return "FizzBuzz";
+  if (number % 5 === 0) return "Buzz";
+  return number;
+};
+
+console.log("a", fizzBuzz("a"));
+for (let i = 0; i <= 100; i++) {
+  console.log(i, fizzBuzz(i));
+}
