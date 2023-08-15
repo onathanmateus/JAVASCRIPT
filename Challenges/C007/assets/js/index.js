86f73080f6fe3fa49fcbe097daacd8a09f38f1cf
Nathan Mateus <117992913+onathanmateus@users.noreply.github.com>
2023-08-15 17:13:07 -0300

    Feat: index.js updated

--- Challenges/C007/assets/js/index.js
+++ Challenges/C007/assets/js/index.js
@@ -1,3 +1,48 @@
 const inputTask = document.querySelector(".input-task");
-const addTask = document.querySelector(".add-task");
+const btnAddTask = document.querySelector(".btn-add-task");
 const tasks = document.querySelector(".tasks");
+
+btnAddTask.addEventListener("click", function () {
+  if (!inputTask.value) return;
+  createTask(inputTask.value);
+});
+
+inputTask.addEventListener("keypress", function (e) {
+  if (e.keyCode === 13) {
+    if (!inputTask.value) return;
+    createTask(inputTask.value);
+  }
+});
+
+document.addEventListener("click", function (e) {
+  const el = e.target;
+  if (el.classList.contains("clear-button")) {
+    el.parentElement.remove();
+  }
+});
+
+const createTask = (inputText) => {
+  const li = createLi();
+  li.innerHTML = inputText;
+  tasks.appendChild(li);
+  clearInput();
+  createClearButton(li);
+};
+
+const createClearButton = (li) => {
+  li.innerHTML += " ";
+  const clearButton = document.createElement("button");
+  clearButton.innerHTML = "Apagar";
+  clearButton.setAttribute("class", "clear-button");
+  li.appendChild(clearButton);
+};
+
+const clearInput = () => {
+  inputTask.value = "";
+  inputTask.focus();
+};
+
+const createLi = () => {
+  const li = document.createElement("li");
+  return li;
+};
