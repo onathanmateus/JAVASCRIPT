fec508881bedf9616421d84c42b4a0c7a67204c9
Nathan Mateus <117992913+onathanmateus@users.noreply.github.com>
2023-08-31 10:15:50 -0300

    feat: C008 - project finalized (the calculator)

--- Challenges/C008/assets/js/index.js
+++ Challenges/C008/assets/js/index.js
@@ -1,54 +1,39 @@
-function createCalculator() {
-  return {
-    display: document.querySelector(".display"),
-    begin() {
-      this.buttonClick();
-    },
-    clearDisplay() {
-      this.display.value = "";
-    },
-    deleteOne() {
-      this.display.value = this.display.value.slice(0, -1);
-    },
-    buttonOnDisplay(value) {
-      this.display.value += value;
-    },
+class Calculator {
+  constructor() {
+    this.display = document.querySelector(".display");
 
-    doACalculation() {
-      let calculation = this.display.value;
+    this.begin = () => this.buttonClick();
+
+    this.buttonClick = () => {
+      document.addEventListener("click", (e) => {
+        const el = e.target;
+        if (el.classList.contains("btn-num")) this.buttonOnDisplay(el);
+        if (el.classList.contains("btn-clear")) this.clearDisplay();
+        if (el.classList.contains("btn-del")) this.deleteOneFromDisplay();
+        if (el.classList.contains("btn-eq")) this.doACalculation();
+      });
+    };
+
+    this.buttonOnDisplay = (el) => (this.display.value += el.innerText);
+    this.clearDisplay = () => (this.display.value = "");
+    this.deleteOneFromDisplay = () => (this.display.value = this.display.value.slice(0, -1));
+    this.doACalculation = () => {
       try {
-        calculation = eval(calculation);
+        const calculation = eval(this.display.value);
         if (!calculation) {
           alert("Conta Inválida !!!");
           this.display.value = "";
           return;
         }
-        this.display.value = String(calculation);
+        this.display.value = calculation;
       } catch (error) {
         alert("Conta Inválida !!!");
         this.display.value = "";
+        return;
       }
-    },
-
-    buttonClick() {
-      document.addEventListener("click", (e) => {
-        const el = e.target;
-        if (el.classList.contains("btn-num")) {
-          this.buttonOnDisplay(el.innerText);
-        }
-        if (el.classList.contains("btn-clear")) {
-          this.clearDisplay();
-        }
-        if (el.classList.contains("btn-del")) {
-          this.deleteOne();
-        }
-        if (el.classList.contains("btn-eq")) {
-          this.doACalculation();
-        }
-      });
-    },
-  };
+    };
+  }
 }
 
-const calculator = createCalculator();
+const calculator = new Calculator();
 calculator.begin();
